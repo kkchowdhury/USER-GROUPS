f18197749e5d7a1ae53e5b7d78f652718b562414
KISHALOY KAR CHOWDHURY <107786775+kkchowdhury@users.noreply.github.com>
2023-05-29 10:28:41 +0530

    Add files via upload

---
+++ components/UserGroupList.js
@@ -0,0 +1,27 @@
+// src/components/UserGroupList.js
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeUserGroup } from '../store/userGroupsSlice';
+import './UserGroupList.css'; 
+
+const UserGroupList = () => {
+  const userGroups = useSelector(state => state.userGroups);
+  const dispatch = useDispatch();
+
+  const handleRemoveUserGroup = (id) => {
+    dispatch(removeUserGroup(id));
+  };
+
+  return (
+    <ul>
+      {userGroups.map(group => (
+        <li key={group.id}>
+          {group.name}
+          <button onClick={() => handleRemoveUserGroup(group.id)}>Remove</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default UserGroupList;
